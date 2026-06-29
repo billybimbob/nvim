58fb4b36e52a4d93cfb64131e6cce9d4c7933936
billyimbob <noahluvsmilkduds@gmail.com>
2026-06-28 23:49:23 -0500

    update vue lsp config

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -2,14 +2,46 @@ vim.pack.add{
 	'https://github.com/neovim/nvim-lspconfig'
 }
 
+vim.lsp.config('lua_ls', {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
+})
+
+local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+local vue_plugin = {
+	name = '@vue/typescript-plugin',
+	location = vue_language_server_path,
+	languages = { 'vue' },
+	configNamespace = 'typescript'
+}
+
+vim.lsp.config('vtsls', {
+	filetypes = { 'typescript', 'javascript', 'javscriptreact', 'typescriptreact', 'vue' },
+	settings = {
+		vtsls = {
+			tsserver = {
+				globalPlugins = {
+					vue_plugin
+				}
+			}
+		}
+	}
+})
+
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('roslyn_ls')
 
 vim.lsp.enable('html')
 vim.lsp.enable('cssls')
-vim.lsp.enable('jsonls')
-vim.lsp.enable('ts_ls')
 
+vim.lsp.enable('jsonls')
+vim.lsp.enable('vtsls')
 vim.lsp.enable('vue_ls')
 vim.lsp.enable('eslint')
 
--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-	'https://github.com/mason-org/mason.nvim.git'
+	{ name = 'mason', src = 'https://github.com/mason-org/mason.nvim' }
 })
 
 require('mason').setup()

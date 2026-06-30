27382c4ec994c250e21cf9a99f5cc4771f91ce16
billyimbob <noahluvsmilkduds@gmail.com>
2026-06-30 16:44:42 -0500

    pivoting again to another color

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
+    { name= 'tokyodark', src = 'https://github.com/tiagovla/tokyodark.nvim' },
     { name = 'rose-pine', src = 'https://github.com/rose-pine/neovim' },
-    'https://github.com/sainnhe/sonokai'
+    'https://github.com/sainnhe/sonokai',
 })
 
-vim.cmd.colorscheme('sonokai')
+vim.cmd.colorscheme('tokyodark')

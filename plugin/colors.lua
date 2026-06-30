4a1405d6f8ad03dc07990ed46294cba148963021
billyimbob <noahluvsmilkduds@gmail.com>
2026-06-30 15:57:30 -0500

    swap color scheme

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,7 +1,6 @@
 vim.pack.add({
-    { name = 'rose-pine', src = 'https://github.com/rose-pine/neovim' }
+    { name = 'rose-pine', src = 'https://github.com/rose-pine/neovim' },
+    'https://github.com/sainnhe/sonokai'
 })
 
-require('rose-pine').setup()
-
-vim.cmd.colorscheme('rose-pine')
+vim.cmd.colorscheme('sonokai')

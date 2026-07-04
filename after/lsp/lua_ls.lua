f224452a1a1b8374e8ae7b18664b7bae2bfde9eb
billybimbob <noahluvsmilkduds@gmail.com>
2026-07-04 02:13:40 -0500

    lsp style tweaks

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,43 +1,46 @@
----@type vim.lsp.Config
-return {
-    on_init = function(client)
-        if not client.workspace_folders then
-            return
-        end
+---@param client vim.lsp.Client
+local function init_neovim_settings(client)
+    if not client.workspace_folders then
+        return
+    end
 
-        local path = client.workspace_folders[1].name
-        local is_config = path == vim.fn.stdpath('config')
-        local has_rc = vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')
+    local path = client.workspace_folders[1].name
+    local is_config = path == vim.fn.stdpath('config')
+    local has_rc = vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')
 
-        if not is_config and has_rc then
-            return
-        end
+    if not is_config and has_rc then
+        return
+    end
 
-        ---@type lspconfig.settings.lua_ls
-        local config_settings = client.config.settings
+    ---@type lspconfig.settings.lua_ls
+    local config_settings = client.config.settings
 
-        ---@type lspconfig.settings.lua_ls
-        local neovim_settings = {
-            Lua = {
-                runtime = {
-                    version = 'LuaJIT',
-                    path = {
-                        'lua/?.lua',
-                        'lua/?/init.lua',
-                    },
+    ---@type lspconfig.settings.lua_ls
+    local neovim_settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
                 },
-                workspace = {
-                    checkThirdParty = false,
-                    library = {
-                        vim.env.VIMRUNTIME,
-                        vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1],
-                    },
-                }
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    vim.api.nvim_get_runtime_file('lua/lspconfig', false)[1],
+                },
             }
         }
+    }
 
-        config_settings.Lua = vim.tbl_deep_extend('force', config_settings.Lua, neovim_settings.Lua)
-    end,
+    config_settings.Lua = vim.tbl_deep_extend('force', config_settings.Lua, neovim_settings.Lua)
+end
+---
+---@type vim.lsp.Config
+return {
+    on_init = init_neovim_settings,
     settings = {
         Lua = {}
     },

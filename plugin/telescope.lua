f6d6507bccdf23e85ee20bcdd7b32e7d7145dd50
billybimbob <noahluvsmilkduds@gmail.com>
2026-07-26 07:12:22 -0500

    forgot fonts no working

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -5,7 +5,7 @@
 
 vim.pack.add({
     'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/nvim-tree/nvim-web-devicons',
+    -- 'https://github.com/nvim-tree/nvim-web-devicons',
     'https://github.com/nvim-telescope/telescope-ui-select.nvim',
     'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
     { name = 'telescope', src = 'https://github.com/nvim-telescope/telescope.nvim' },

236ac0ce5e3d10b9cc67b709aa2a63666a9b9182
billybimbob <noahluvsmilkduds@gmail.com>
2026-09-03 14:49:55 -0500

    handle eslint formatting conflicts in the global attach

--- after/lsp/eslint.lua
+++
@@ -1,9 +0,0 @@
----@type vim.lsp.Config
-return {
-    settings = {
-        format = true
-    },
-    on_init = function(client)
-        client.server_capabilities.documentFormattingProvider = true
-    end
-}

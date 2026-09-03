24084ee1a7e1ce7d9e5bdffbbee38c26868d3a31
billybimbob <noahluvsmilkduds@gmail.com>
2026-09-03 00:43:58 -0500

    format with eslint

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,5 +3,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { buf = ev.buf })
         vim.keymap.set('n', '<leader>gh', vim.diagnostic.open_float, { buf = ev.buf })
         vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buf = ev.buf })
+        vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { buffer = ev.buf })
     end
 })

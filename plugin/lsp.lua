3ca18ab5cb79e61df6166204c1b9c064a030b4f8
billybimbob <noahluvsmilkduds@gmail.com>
2026-09-18 00:28:59 -0500

    maybe we dont need autotrigger

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -7,15 +7,7 @@ local function attach_lsp_modifiers(ev)
     local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
 
     if client:supports_method('textDocument/completion', ev.buf) then
-        local all_triggers = {}
-        for i = 32, 126 do
-            table.insert(all_triggers, string.char(i))
-        end
-
-        client.server_capabilities.completionProvider.triggerCharacters = all_triggers
-
         vim.lsp.completion.enable(true, client.id, ev.buf, {
-            autotrigger = true,
             convert = function(item)
                 return { abbr = item.label:gsub('%b()', '') }
             end
